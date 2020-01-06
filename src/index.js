690ddfcce333465da9dd93ccf9433f0a7d87c197
Didin1453fatih <didin1453fatih@gmail.com>
2020-01-06 12:34:55 +0700

    add description

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import btoa from "btoa";
 import printJS from "print-js";
 import axios from "axios";
 
-module.exports.printPdf = async function (url, data, finishCb) {
+/**
+ * 
+ * @param {*} url is string 
+ * @param {*} data is object type
+ * @param {*} finishCb finish callback when pdf rendered in print popup
+ */
+export const printPdf = async function (url, data, finishCb) {
+    // console.log('print pdf start 2 '+JSON.stringify(data))
     var response = await axios.post(
         url,
         {
-            data_pdf: btoa(data)
+            data_pdf: btoa(JSON.stringify(data))
         },
         {
             responseType: "arraybuffer"
         }
     );
+    // console.log('print axios')    
     var filePdf = Buffer.from(response.data, "binary").toString("base64");
-
     await printJS({
         printable: filePdf,
         type: "pdf",
